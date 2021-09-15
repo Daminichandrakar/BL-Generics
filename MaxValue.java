7ab649a748ecbeaf1e0fa3194bd2ce51b749fcd1
Daminichandrakar <damini.2201@gmail.com>
2021-09-15 13:54:40 +0530

    Refector: create generic class

--- MaxValue.java
+++ MaxValue.java
@@ -1,8 +1,16 @@
 package com.bl;
 
-public class MaxValue {
+public class MaxValue<T extends Comparable<T>> {
 
-	public <T extends Comparable<T>> T findMax(T value1, T value2, T value3) {
+	T value1, value2, value3;
+
+	public MaxValue(T value1, T value2, T value3) {
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
+
+	public static <T extends Comparable<T>> T testMaximum(T value1, T value2, T value3) {
 		T max = value1;
 		if (value2.compareTo(max) > 0) {
 			max = value2;
@@ -12,5 +20,9 @@ public class MaxValue {
 		}
 		System.out.println("Maximum value is " + max);
 		return max;
+	}
+
+	public T findMax() {
+		return MaxValue.testMaximum(value1, value2, value3);
 	}
 }
--- MaxValueTestCase.java
+++ MaxValueTestCase.java
@@ -5,60 +5,57 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class MaxValueTestCase {
-	MaxValue max = new MaxValue();
-
 	@Test
 	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtFirstPosition() {
-		int value = max.findMax(9, 5, 6);
+		int value = new MaxValue<Integer>(9, 5, 4).findMax();
 		Assert.assertEquals(9, value);
 	}
 
 	@Test
 	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtSecondPosition() {
-		int value = max.findMax(5, 9, 6);
+		int value = new MaxValue<Integer>(5, 9, 6).findMax();
 		Assert.assertEquals(9, value);
 	}
 
 	@Test
 	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtThirdPosition() {
-		int value = max.findMax(6, 5, 9);
+		int value = new MaxValue<Integer>(6, 5, 9).findMax();
 		Assert.assertEquals(9, value);
 	}
 
 	@Test
 	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxAtFirstPosition() {
-		float value = max.findMax(9.0f, 5.0f, 6.0f);
+		float value = new MaxValue<Float>(9.0f, 5.0f, 6.0f).findMax();
 		Assert.assertEquals(9.0f, value, 0.0f);
 	}
 
 	@Test
 	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxSecoundPosition() {
-		float value = max.findMax(5.0f, 9.0f, 6.0f);
+		float value = new MaxValue<Float>(5.0f, 9.0f, 6.0f).findMax();
 		Assert.assertEquals(9.0f, value, 0.0f);
 	}
 
 	@Test
 	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxThirdPosition() {
-		float value = max.findMax(9.0f, 5.0f, 11.0f);
+		float value = new MaxValue<Float>(9.0f, 5.0f, 11.0f).findMax();
 		Assert.assertEquals(11.0f, value, 0.0f);
 	}
 
 	@Test
 	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtFirstPosition() {
-		String value = max.findMax("peach", "apple", "banana");
+		String value = new MaxValue<String>("peach", "apple", "banana").findMax();
 		Assert.assertEquals("peach", value);
 	}
 
 	@Test
 	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtSecoundPosition() {
-		String value = max.findMax("apple", "peach", "banana");
+		String value = new MaxValue<String>("apple", "peach", "banana").findMax();
 		Assert.assertEquals("peach", value);
 	}
 
 	@Test
 	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtThirdPosition() {
-		String value = max.findMax("apple", "banana", "peach");
+		String value = new MaxValue<String>("apple", "banana", "peach").findMax();
 		Assert.assertEquals("peach", value);
 	}
-
 }
