5536397beb43beb35e0c8f34d12c0e03253fe14b
Daminichandrakar <damini.2201@gmail.com>
2021-09-15 12:52:15 +0530

    Added: Take 3 float value and find max and Write test case

--- MaxValueTestCase.java
+++ MaxValueTestCase.java
@@ -25,4 +25,22 @@ class MaxValueTestCase {
 		Assert.assertEquals(9, value);
 	}
 
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxAtFirstPosition() {
+		float value = max.findMax(9.0f, 5.0f, 6.0f);
+		Assert.assertEquals(9.0f, value, 0.0f);
+	}
+
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxSecoundPosition() {
+		float value = max.findMax(5.0f, 9.0f, 6.0f);
+		Assert.assertEquals(9.0f, value, 0.0f);
+	}
+
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxThirdPosition() {
+		float value = max.findMax(9.0f, 5.0f, 11.0f);
+		Assert.assertEquals(11.0f, value, 0.0f);
+	}
+
 }
