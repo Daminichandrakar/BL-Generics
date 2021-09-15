d9bf75c9a5830bfbaaca3538bf8f8fdf216de813
Daminichandrakar <damini.2201@gmail.com>
2021-09-15 13:02:09 +0530

    Refector: all 3 method to one generic method

--- MaxValue.java
+++ MaxValue.java
@@ -1,43 +1,16 @@
 package com.bl;
 
 public class MaxValue {
-	// Find integer maximum value
-	public Integer findMax(Integer value1, Integer value2, Integer value3) {
-		Integer max = value1;
+
+	public <T extends Comparable<T>> T findMax(T value1, T value2, T value3) {
+		T max = value1;
 		if (value2.compareTo(max) > 0) {
 			max = value2;
 		}
 		if (value3.compareTo(max) > 0) {
 			max = value3;
 		}
-		System.out.println("Maximum Integer number is " + max);
+		System.out.println("Maximum value is " + max);
 		return max;
 	}
-
-	// Find float maximum value
-	public Float findMax(Float value1, Float value2, Float value3) {
-		Float max = value1;
-		if (value2.compareTo(max) > 0) {
-			max = value2;
-		}
-		if (value3.compareTo(max) > 0) {
-			max = value3;
-		}
-		System.out.println("Maximum Float number is " + max);
-		return max;
-	}
-
-	// Find float maximum value
-	public String findMax(String value1, String value2, String value3) {
-		String max = value1;
-		if (value2.compareTo(max) > 0) {
-			max = value2;
-		}
-		if (value3.compareTo(max) > 0) {
-			max = value3;
-		}
-		System.out.println("Maximum String number is " + max);
-		return max;
-	}
-
 }
